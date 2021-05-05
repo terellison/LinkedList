2b4295c2404749aab6f25f6da845bbfd6dc4089c
terellison <ellisonterry4@gmail.com>
2021-05-05 17:53:52 -0400

    added Teardown and Load methods

--- LinkedList.Test/LinkedList.Test.cpp
+++ LinkedList.Test/LinkedList.Test.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "../LinkedList.h"
+#include "../LinkedList/LinkedList.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,12 +9,25 @@ namespace LinkedListTest
 	TEST_CLASS(LinkedListTest)
 	{
 		LinkedList::LinkedList<int>* linkedList;
+
+		void Load() {
+			int* ptr = 0;
+			for(int i = 1; i < 200; i++) {
+				ptr = &i;
+				this->linkedList->append(ptr);
+			}
+		}
+
+		void Teardown() {
+			delete this->linkedList;
+		}
 	public:
 		
 		TEST_METHOD(Is_LinkedList_Empty)
 		{
 			this->linkedList = new LinkedList::LinkedList<int>;
-			Assert::IsTrue(this->linkedList->Empty());
+			Assert::IsTrue(this->linkedList->isEmpty());
+			Teardown();
 		}
 
 		TEST_METHOD(LinkedList_Is_Not_Empty_After_Append) {
@@ -23,7 +36,10 @@ namespace LinkedListTest
 			int testNum = 1;
 			testPtr = &testNum;
 			this->linkedList->append(testPtr);
-			Assert::IsFalse(this->linkedList->Empty());
+			Assert::IsFalse(this->linkedList->isEmpty());
+			Teardown();
 		}
+
+
 	};
 }
